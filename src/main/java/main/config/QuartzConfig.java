cfa77d35c77a00268eb9336b6fabfd9a980b532a
Ort <orthodoxplus@ya.ru>
2023-04-15 02:33:12 +0300

    dont wait unlock http

--- src/main/java/main/config/QuartzConfig.java
+++ src/main/java/main/config/QuartzConfig.java
@@ -1,20 +1,15 @@
 package main.config;
 
 import main.quartz.ScanJob;
-import main.quartz.WaitingUnlockHttpJob;
 import org.quartz.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import static org.quartz.SimpleScheduleBuilder.repeatHourlyForever;
-import static org.quartz.SimpleScheduleBuilder.repeatMinutelyForever;
 
 @Configuration
 public class QuartzConfig {
-	@Value("${habr.minutes_to_wait_unlock_habr_http}")
-	private int minutesToWaitUnlockHabrHttp;
 	@Bean
 	public Scheduler scheduler(SchedulerFactoryBean factory) throws SchedulerException {
 		TriggerKey scanJob = TriggerKey.triggerKey(ScanJob.class.getName());
@@ -27,23 +22,9 @@ public class QuartzConfig {
 				.withIdentity(scanJob)
 				.build();
 
-
-		TriggerKey unlockHttpJob = TriggerKey.triggerKey(WaitingUnlockHttpJob.class.getName());
-		JobDetail waitingUnlockHttpJobDetail = JobBuilder.newJob()
-				.storeDurably()
-				.ofType(WaitingUnlockHttpJob.class)
-				.build();
-		SimpleTrigger waitingUnlockHttpTrigger = TriggerBuilder.newTrigger()
-				.withSchedule(repeatMinutelyForever(minutesToWaitUnlockHabrHttp))
-				.withIdentity(unlockHttpJob)
-				.build();
-
 		Scheduler scheduler = factory.getScheduler();
 		scheduler.scheduleJob(scanJobDetail, scanTrigger);
-		scheduler.scheduleJob(waitingUnlockHttpJobDetail, waitingUnlockHttpTrigger);
 		scheduler.start();
-
-		scheduler.pauseTrigger(unlockHttpJob);
 		return scheduler;
 	}
 }
